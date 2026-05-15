e08aa1cf2a3ba18db280231fbf0a54f495c08268
Fabian H. <hensslerfabian@gmail.com>
2026-05-15 11:50:55 +0200

    add 3 networks

--- Nets.py
+++ Nets.py
@@ -18,8 +18,9 @@ class Base_Net(torch.nn.Module):
         self.conv5 = torch.nn.Conv2d(
             in_channels=32, out_channels=16, kernel_size=3, padding=1)
         self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        self.global_avg_pool = torch.nn.AdaptiveAvgPool2d((32, 32))
         # Linear -> fully connected
-        self.fc1 = torch.nn.Linear(16*64*64, 128)
+        self.fc1 = torch.nn.Linear(16*32*32, 128)
         self.fc2 = torch.nn.Linear(128, 128)
         # self.fc2 = torch.nn.Linear(120, 84)
         self.fc3 = torch.nn.Linear(128, NUM_CLASSES)
@@ -30,6 +31,140 @@ class Base_Net(torch.nn.Module):
         x = torch.nn.functional.relu(self.conv2(x))
         x = self.pool(x)
         x = torch.nn.functional.relu(self.conv3(x))
+        x = torch.nn.functional.relu(self.conv4(x))
+        x = self.pool(x)
+        x = torch.nn.functional.relu(self.conv5(x))
+        x = self.global_avg_pool(x)
+
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class Conv_Net(torch.nn.Module):
+    # define layer
+    def __init__(self):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv3 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv4 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv5 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv6 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv7 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv8 = torch.nn.Conv2d(
+            in_channels=32, out_channels=16, kernel_size=3, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        # Linear -> fully connected
+        self.fc1 = torch.nn.Linear(16*64*64, 128)
+        self.fc2 = torch.nn.Linear(128, 128)
+        # self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(128, NUM_CLASSES)
+
+    # define data flow / architecture
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.conv1(x))
+        x = torch.nn.functional.relu(self.conv2(x))
+        x = torch.nn.functional.relu(self.conv3(x))
+        x = self.pool(x)
+        x = torch.nn.functional.relu(self.conv4(x))
+        x = torch.nn.functional.relu(self.conv5(x))
+        x = torch.nn.functional.relu(self.conv6(x))
+        x = self.pool(x)
+        x = torch.nn.functional.relu(self.conv7(x))
+        x = torch.nn.functional.relu(self.conv8(x))
+
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class Dense_Net(torch.nn.Module):
+    # define layer
+    def __init__(self):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv3 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv4 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv5 = torch.nn.Conv2d(
+            in_channels=32, out_channels=16, kernel_size=3, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        # Linear -> fully connected
+        self.fc1 = torch.nn.Linear(16*64*64, 128)
+        self.fc2 = torch.nn.Linear(128, 128)
+        self.fc3 = torch.nn.Linear(128, 128)
+        self.fc4 = torch.nn.Linear(128, 128)
+        self.fc5 = torch.nn.Linear(128, 128)
+        self.fc6 = torch.nn.Linear(128, 128)
+        # self.fc2 = torch.nn.Linear(120, 84)
+        self.fc7 = torch.nn.Linear(128, NUM_CLASSES)
+
+    # define data flow / architecture
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.conv1(x))
+        x = torch.nn.functional.relu(self.conv2(x))
+        x = self.pool(x)
+        x = torch.nn.functional.relu(self.conv3(x))
+        x = torch.nn.functional.relu(self.conv4(x))
+        x = self.pool(x)
+        x = torch.nn.functional.relu(self.conv5(x))
+
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = torch.nn.functional.relu(self.fc3(x))
+        x = torch.nn.functional.relu(self.fc4(x))
+        x = torch.nn.functional.relu(self.fc5(x))
+        x = torch.nn.functional.relu(self.fc6(x))
+        x = self.fc7(x)
+        return x
+
+
+class Pooling_Net(torch.nn.Module):
+    # define layer
+    def __init__(self):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv3 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv4 = torch.nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv5 = torch.nn.Conv2d(
+            in_channels=32, out_channels=16, kernel_size=3, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        # Linear -> fully connected
+        self.fc1 = torch.nn.Linear(16*16*16, 128)
+        self.fc2 = torch.nn.Linear(128, 128)
+        # self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(128, NUM_CLASSES)
+
+    # define data flow / architecture
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.conv1(x))
+        x = self.pool(x)
+        x = torch.nn.functional.relu(self.conv2(x))
+        x = self.pool(x)
+        x = torch.nn.functional.relu(self.conv3(x))
+        x = self.pool(x)
         x = torch.nn.functional.relu(self.conv4(x))
         x = self.pool(x)
         x = torch.nn.functional.relu(self.conv5(x))
